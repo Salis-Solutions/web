94fcf0b2846c009b1b3e5beeaf78f154daff6e8c
Salis-Solutions <info.salissolutions@gmail.com>
2026-07-07 12:47:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Change navbar background while scrolling
+
+window.addEventListener("scroll", function(){
+
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 80){
+
+        header.style.background = "rgba(0,0,0,0.85)";
+
+    }
+    else{
+
+        header.style.background = "rgba(0,0,0,0.35)";
+
+    }
+
+});
